0f22496f034422e52fa794353bc5a24aa647fe09
Lily Lee <lilylee88756@gmail.com>
2021-07-13 11:41:18 +0800

    add 0005

--- algorithms/0005/main.go
+++ algorithms/0005/main.go
@@ -1,5 +1,41 @@
 package _0005
 
 func longestPalindrome(s string) string {
-	return s
+	if len(s) == 1 {
+		return s
+	}
+	str := s[0:1]
+	if len(s) == 2 {
+		if s[0] == s[1] {
+			return s
+		}
+		return str
+	}
+	for i := 0; i < len(s); i++ {
+		j := 2
+		for j <= len(s) && i <= len(s)-j {
+			sub := s[i : i+j]
+			if check(sub) {
+				if len(sub) > len(str) {
+					str = sub
+				}
+			}
+			j++
+		}
+	}
+
+	return str
+}
+
+func check(s string) bool {
+	mid := len(s) / 2
+	if len(s)%2 == 0 {
+		mid = mid - 1
+	}
+	for i := 0; i <= mid; i++ {
+		if !(s[i] == s[len(s)-i-1]) {
+			return false
+		}
+	}
+	return true
 }
